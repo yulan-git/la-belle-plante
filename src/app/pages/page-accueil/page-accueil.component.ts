3469c0c2ceb7a2af20d547c1d71eace9cd372f27
Lafferrière <flora.lafferriere@gmail.com>
2022-01-04 10:22:58 +0100

    work in progress

--- src/app/pages/page-accueil/page-accueil.component.ts
+++ src/app/pages/page-accueil/page-accueil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { list_products } from 'src/app/data';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-page-accueil',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-accueil.component.scss']
 })
 export class PageAccueilComponent implements OnInit {
+  private data: any[];
+  public listCategories: string[];
 
-  constructor() { }
+  constructor() {
+    this.data = list_products;
+    this.listCategories = _.uniq(this.data.map(x => x.product_breadcrumb_label));
+    console.log(this.listCategories);
+  }
 
   ngOnInit(): void {
   }
